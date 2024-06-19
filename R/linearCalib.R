f43edf94a0135a4ede687af928deccbd0de23a5d
Pablo Martínez Sosa <martoza@gmail.com>
2024-06-19 16:24:43 +0200

    Added index recognition for linearCalib
    Now the function linearCalib can input index (MBT'5Me or CBT') for the calculations.

--- R/linearCalib.R
+++ R/linearCalib.R
@@ -4,6 +4,7 @@
 #' @param gdgt Data frame with the peak areas or fractional abundances of GDGTs. Each row should correspond to a sample and each column to each GDGT with the appropriated name format.
 #' @param env Selection of the environmental parameter to reconstruct. Options currently are "Temperature" and "pH"
 #' @param calibration Selection of the forward model to be used. Currently options are "Russell" (Russell, 2018), "Martinez" (Martinez-Sosa, 2022), "DeJonge" (De Jonge, 2014), and "Naafs" (Naafs, 2017) for temperature; "Russell" (Russell, 2018), and "DeJonge" (De Jonge, 2014) for pH.
+#' @param data Data is presented as either peak areas ("raw") or a vector with the corresponding index values for the calibration ("index").
 #' @param complete Logic variable. Indicates whether all brGDGTs in the original formulation are present in the data frame (TRUE, default value) or if some are missing. If TRUE the function will not proceed if any variable is missing, it will produce an error. If FALSE, the function will remove the missing variables from the equation.
 #' @param na.ignore Logic variable. If FALSE (default) the function will turn any NA values into 0 for the calculations.
 #' @return The function returns the MBT'5Me value for each sample provided.
@@ -12,7 +13,7 @@
 #' @references De Jonge, C., Hopmans, E. C., Zell, C. I., Kim, J. H., Schouten, S., & Damsté, J. S. S. (2014). Occurrence and abundance of 6-methyl branched glycerol dialkyl glycerol tetraethers in soils: Implications for palaeoclimate reconstruction. Geochimica et Cosmochimica Acta, 141, 97-112.
 #' @references Naafs, B. D. A., Inglis, G. N., Zheng, Y., Amesbury, M. J., Biester, H., Bindler, R., ... & Pancost, R. D. (2017). Introducing global peat-specific temperature and pH calibrations based on brGDGT bacterial lipids. Geochimica et Cosmochimica Acta, 208, 285-301.
 
-linearCalib<-function(gdgt,env,calibration,complete=TRUE,na.ignore=FALSE){
+linearCalib<-function(gdgt,env,calibration,data="raw",complete=TRUE,na.ignore=FALSE){
   ## Calibrations
   #Russell et al., 2018
   AfricaRussellMAAT<-alist(MBT5=,m=32.42,b=-1.21,(MBT5*m)+b)
@@ -29,8 +30,14 @@ linearCalib<-function(gdgt,env,calibration,complete=TRUE,na.ignore=FALSE){
   pH<-list(Russell=AfricaRussellpH,DeJonge=GlobalDeJongepH)
   Calibrations<-list(Temperature=Temperature,pH=pH)
 
-  if(env=="Temperature"){prediction<-as.function(Calibrations[[env]][[calibration]])(MBT5(gdgt,complete = complete,na.ignore=na.ignore))
-  } else if(env=="pH"){prediction<-as.function(Calibrations[[env]][[calibration]])(CBTp(gdgt,complete = complete,na.ignore=na.ignore))
-  } else stop("Select one of the available environmental parameters: Temperature or pH")
+  if(data=="raw"){
+    if(env=="Temperature"){prediction<-as.function(Calibrations[[env]][[calibration]])(MBT5(gdgt,complete = complete,na.ignore=na.ignore))
+    } else if(env=="pH"){prediction<-as.function(Calibrations[[env]][[calibration]])(CBTp(gdgt,complete = complete,na.ignore=na.ignore))
+    } else stop("Select one of the available environmental parameters: Temperature or pH")
+  } else if(data=="index"){
+    if(env=="Temperature"){prediction<-as.function(Calibrations[[env]][[calibration]])(gdgt)
+    } else if(env=="pH"){prediction<-as.function(Calibrations[[env]][[calibration]])(gdgt)
+    } else stop("Select one of the available environmental parameters: Temperature or pH")
+  }
   prediction
 }
