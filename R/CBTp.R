304935f827379aaf377acb71c7c6e9aee938d855
Pablo Martínez Sosa <martoza@gmail.com>
2025-06-23 19:49:21 +0200

    Update CBTp.R
    Corrected the CBT' equation which was missing a - for the -log10.

--- R/CBTp.R
+++ R/CBTp.R
@@ -9,7 +9,7 @@
 
 CBTp<-function(gdgt,complete=TRUE,na.ignore=FALSE){
   if(na.ignore==TRUE){gdgt[is.na(gdgt)]<-0}
-  formula<-"log10(( gdgt$Ic + gdgt$IIa6 + gdgt$IIb6 + gdgt$IIc6 + gdgt$IIIa6 + gdgt$IIIb6 + gdgt$IIIc6 )/( gdgt$Ia + gdgt$IIa5 + gdgt$IIIa5 ))"
+  formula<-"-log10(( gdgt$Ic + gdgt$IIa6 + gdgt$IIb6 + gdgt$IIc6 + gdgt$IIIa6 + gdgt$IIIb6 + gdgt$IIIc6 )/( gdgt$Ia + gdgt$IIa5 + gdgt$IIIa5 ))"
   if(complete==TRUE){if(NA %in% match(correctGs(formula),colnames(gdgt))){stop("Missing variables")}}
   if(complete==FALSE){formula<-partialEq(gdgt,formula)}
   out<-eval(parse(text = formula))
